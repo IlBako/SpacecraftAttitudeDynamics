bc9df96106fdc7b303c5338057274f9411065dfa
SteBe01 <stefano3.belletti@mail.polimi.it>
2023-12-06 15:02:57 +0100

    Added complete attitude animation

--- Animation/Animation.m
+++ Animation/Animation.m
@@ -18,3 +18,25 @@ animation_length = 30000;    % number of frames displayed (total)
 
 attitudeAnimation(A1, A2, stlName, percent_start, step_size, animation_length);
 
+
+%% complete attitude animation
+
+clc
+close all
+
+if ~exist('out','var')
+    error("Run simulation first!")
+end
+warning("Satellite is not accurate for inertia and size, use for reference only")
+
+A = out.A_BN_Sens;              % attitude
+P = out.kepler_r_vec;           % radius of orbit
+T = out.kepler_theta;           % theta of orbit           
+stlName = "Satellite.STL";
+
+percent_start = 0;              % simulation perc. for animation start. point
+step_size = 50;
+animation_length = 5000;        % number of frames displayed (total)
+
+attitudeAnimation2(A, P, T, stlName, percent_start, step_size, animation_length, 1);
+
